83fd7573f0f2502de653f364174122c462c5c380
hariom3794 <singhhariom3794@gmail.com>
2024-10-15 00:26:53 +0530

    Add files via upload

---
+++ income tax calculator/src/Main.java
@@ -0,0 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
+class CalculatorForIncomeTax{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter income: ");
+        int income= sc.nextInt();
+        int tax ;
+        if (income<500000){
+            tax=0;
+        }
+        else if(income>=500000&&income<1000000){
+            tax=(int)(income*0.2);
+        }
+        else{
+            tax=(int)(income*0.3);
+        }
+
+        System.out.print("your tax willbe : "+tax);
+
+        }
+
+}
